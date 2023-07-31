76fa07880c982f88059586d2a74ec2264cf861f2
Caleb Kliewer <cfkliewer@gmail.com>
2023-07-31 13:20:47 -0500

    enable ssl

--- app/api/send-mail/route.ts
+++ app/api/send-mail/route.ts
@@ -19,6 +19,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
 
 	let transporter = nodemailer.createTransport({
 		service: 'Gmail',
+		secure: true,
 		auth: {
 			user: process.env.EMAIL_USER,
 			pass: process.env.EMAIL_PASS
